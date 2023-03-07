bdad756dedba134d071542ec1911c95a8221d077
fannyl6720 <fanny.liang23@sitechhs.com>
2023-03-07 00:17:42 -0500

    change background color

--- create-task/main.js
+++ create-task/main.js
@@ -1,154 +1,52 @@
-/* import "./style.css"; */
-import { setupCounter } from "./counter.js";
-import { HttpProxy } from "vite";
+import './style.css'
+import { setupCounter } from './counter.js'
 
-/* const apiEntry =
-  "https://metmuseum.org/art/collection/search#!?q=test&offset=0&pageSize=0&sortBy=Relevance&sortOrder=asc&perPage=20";
-console.log(fetch(apiEntry));
-const form = document.querySelector(".search-form");
-const imageResults = document.querySelector(".image-container");
+let colorsArray = [`#AC1F20`, `#D82526`, `#E24F50`, `#EA7A7A`, `#F1A5A6`];
+let isFeched = false;
+document.getElementById("color-scheme-button").addEventListener("click", () => {
+  const colorHex = document.getElementById("color-input").value.slice(1);
+  const colorScheme = document
+    .getElementById("select-mode")
+    .value.toLowerCase();
 
-fetch(apiEntry)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colorHex}&mode=${colorScheme}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      colorsArray = data.colors;
+      isFeched = true;
+      render(colorsArray);
+    });
+});
 
-async function fetchData(apiEntry) {
-  try {
-    const response = await fetch(apiEntry);
-    const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
+function render(data) {
+  let html = "";
+
+  data.forEach((item) => {
+    const hexCode = isFeched ? item.hex.value : item;
+    html += `
+          <div class="pallete" style="background:${hexCode}"> 
+                  <span class="hex-color" data-hex="${hexCode}" id=${hexCode}>${hexCode}</span>
+                </div>
+        `;
+  });
+  document.getElementById("color-scheme-container").innerHTML = html;
 }
-fetchData(apiEntry);
+render(colorsArray);
 
-async function fetchText() {
-  let response = await fetch("/readme.txt");
+document
+  .querySelector("#color-scheme-container")
+  .addEventListener("click", (e) => {
+    if (e.target.dataset.hex) {
+      const copyHex = e.target.dataset.hex;
+      // navigator.clipboard.writeText(copyHex);
 
-  console.log(response.status);
-  console.log(response.statusText);
-
-  if (response.status === 200) {
-    let data = await response.text();
-  }
-}
-
-fetchText();
-
-let response = await fetch("/non-existence.txt");
-
-console.log(response.status);
-console.log(response.statusText);
-
-async function load_pic() {
-  const url =
-    "https://metmuseum.org/art/collection/search#!?q=test&offset=0&pageSize=0&sortBy=Relevance&sortOrder=asc&perPage=20";
-
-  const options = {
-    method: "GET",
-  };
-
-  let response = await fetch(url, options);
-
-  if (response.status === 200) {
-    const imageBlob = await response.blob();
-    const imageObjectURL = URL.createObjectURL(imageBlob);
-
-    const image = document.createElement("img");
-    image.src = imageObjectURL;
-
-    const container = document.getElementById("your-container");
-    container.append(image);
-  } else {
-    console.log("HTTP-Error: " + response.status);
-  }
-}
- */
-
-/* fetch(
-  "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1"
-)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
+      document.getElementById(`${copyHex}`).textContent = "copied!!";
+      document.getElementById(`${copyHex}`).style.background = "#f4f4f4";
+      setTimeout(() => {
+        document.getElementById(`${copyHex}`).textContent = copyHex;
+        document.getElementById(`${copyHex}`).style.background = "#fff";
+      }, 1000);
     }
-  })
-  .then((data) => {
-    console.log(data);
-    displayCocktail(data);
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-
-function displayCocktail(data) {
-  const cocktail = data.drinks[0];
-  const cocktailDiv = document.getElementById("cocktail");
-
-  const cocktailName = cocktail.strDrink;
-  const heading = document.createElement("h1");
-  heading.innerHTML = cocktailName;
-  cocktailDiv.appendChild(heading);
-
-  const cocktailImg = document.createElement("img");
-  cocktailImg.src = cocktail.strDrinkThumb;
-  cocktailDiv.appendChild(cocktailImg);
-  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
-
-  const cocktailIngredients = document.createElement("ul");
-  cocktailDiv.appendChild(cocktailIngredients);
-  const getIngredients = Object.keys(cocktail)
-    .filter(function (ingredient) {
-      return ingredient.indexOf("strIngredient") == 0;
-    })
-    .reduce(function (ingredients, ingredient) {
-      if (cocktail[ingredient] != null) {
-        ingredients[ingredient] = cocktail[ingredient];
-      }
-      return ingredients;
-    }, {});
-  for (let key in getIngredients) {
-    let value = getIngredients[key];
-    listItem = document.createElement("li");
-    listItem.innerHTML = value;
-    cocktailIngredients.appendChild(listItem);
-  }
-} */
-
-const api_url = "https://employeedetails.free.beeceptor.com/my/api/path";
-
-async function getapi(url) {
-  const response = await fetch(url);
-  let data = await response.json();
-  console.log(data);
-  if (response) {
-    hideloader();
-  }
-  show(data);
-}
-getapi(api_url);
-
-function hideloader() {
-  document.getElementById("loading").style.display = "";
-}
-
-function show(data) {
-  let tab = `<tr>
-        <th>Name</th>
-        <th>Office</th>
-        <th>Position</th>
-        <th>Salary</th>
-       </tr>`;
-
-  for (let r of data.list) {
-    tab += `<tr> 
-  <td>${r.name} </td>
-  <td>${r.office}</td>
-  <td>${r.position}</td> 
-  <td>${r.salary}</td>          
-</tr>`;
-  }
-  document.getElementById("employees").innerHTML = tab;
-}
+  });
